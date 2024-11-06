fd52ee604b82899a63ea805fa73bf1a79e1c3f0e
belka.boo <ibelka.boo@gmail.com>
2024-11-06 20:54:57 +0300

    2_sem 2 task add solution

--- 2_SEM/2_sem.js
+++ 2_SEM/2_sem.js
@@ -28,3 +28,27 @@ menuLinks.forEach(link => {
 });
 
 
+/*
+Создайте простое модальное окно, которое появляется при клике на кнопку "Открыть
+модальное окно" и закрывается при клике на кнопку "Закрыть". Модальное окно
+должно содержать заголовок "Модальное окно" и кнопку для закрытия.
+* Модальное окно должно плавно появляться и исчезать при открытии и закрытии.
+*/
+
+
+const modalEl = document.querySelector('#modal');
+const modalOpenBtn = document.querySelector('.modal-open-btn');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
+
+
+modalOpenBtn.addEventListener('click', function (e) {
+    modalEl.classList.add('active');
+
+});
+
+modalCloseBtn.addEventListener('click', function (e) {
+    modalEl.classList.remove('active');
+});
+
+
+
