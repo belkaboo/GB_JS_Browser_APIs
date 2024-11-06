e083c2d561fffde23c5d320ec5dfcb75567f679a
belka.boo <ibelka.boo@gmail.com>
2024-11-06 21:04:31 +0300

    2_sem 3 task add solution

--- 2_SEM/2_sem.js
+++ 2_SEM/2_sem.js
@@ -50,5 +50,26 @@ modalCloseBtn.addEventListener('click', function (e) {
     modalEl.classList.remove('active');
 });
 
+/*
+У вас есть кнопка "Купить". Создайте скрипт, который при клике на эту кнопку меняет её текст на "Товар
+добавлен в корзину" в течение 2 секунд, а затем возвращает исходный текст "Купить". В обработчике
+события клика также проверьте, является ли событие доверенным. Если событие является доверенным,
+выполните изменение текста кнопки и убедитесь, что после 2 секунд текст возвращается в исходное
+состояние.
+*/
+
+const buyBtn = document.querySelector('.buy-btn');
+
+buyBtn.addEventListener('click', function (e) {
+    if (e.isTrusted) {
+        e.target.textContent = 'Товар добавлен в корзину';
+        buyBtn.disabled = true;
+        setTimeout(() => {
+            e.target.textContent = 'Купить'
+            buyBtn.disabled = false;
+        }, 2000);
+    }
+});
+
 
 
